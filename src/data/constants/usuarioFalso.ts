8fe5710a20e9351dffd20a7e29aa2e4a5fdf7c55
Juliano Moser <juliano.moserjj@gmail.com>
2023-05-14 00:21:34 -0300

    Implementado parte das finanças
    Implementado cabeçalho
    Implementado menu usuário

---
+++ src/data/constants/usuarioFalso.ts
@@ -0,0 +1,8 @@
+import Usuario from "@/logic/core/usuario/Usuario";
+
+export default {
+    id: "123123123",
+    nome: 'João da Silva',
+    email: 'jjjjoao@xmail.com',
+    imagemUrl: null,
+} as Usuario
---
+++ src/pages/usuario.tsx
@@ -0,0 +1,12 @@
+import Cabecalho from "@/components/template/Cabecalho";
+import Pagina from "@/components/template/Pagina";
+
+export default function CadastroUsuario() {
+
+    return (
+        <Pagina>
+            <Cabecalho />
+            <div>Usuário</div>
+        </Pagina>
+    )
+}
