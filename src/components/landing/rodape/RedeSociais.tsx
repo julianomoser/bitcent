c4da43c686918219eb49498460d316a68ed178f2
Juliano Moser <juliano.moserjj@gmail.com>
2023-05-13 23:35:17 -0300

    Implementado rodape

---
+++ src/components/landing/rodape/RedeSociais.tsx
@@ -0,0 +1,12 @@
+import { IconBrandFacebook, IconBrandGithub, IconBrandInstagram, IconBrandLinkedin, IconBrandMatrix, IconBrandYoutube } from "@tabler/icons-react"
+import RedeSocial from "./RedeSocial"
+
+export default function RedesSociais() {
+    return (
+        <div className="flex">
+            <RedeSocial icone={<IconBrandYoutube />} url="https://www.youtube.com/@cod3r" />
+            <RedeSocial icone={<IconBrandLinkedin />} url="https://br.linkedin.com/in/juliano-moser-de-jesus-883baa256" />
+            <RedeSocial icone={<IconBrandGithub />} url="https://github.com/julianomoser" />
+        </div>
+    )
+}
--- src/components/landing/rodape/index.tsx
+++ src/components/landing/rodape/index.tsx
@@ -1,9 +1,26 @@
 import Area from "../comum/Area";
+import Logo from "../comum/Logo";
+import RedesSociais from "./RedeSociais";
+import Link from "next/link"
 
 export default function Rodape() {
     return (
-        <Area>
-
+        <Area className="bg-black py-20">
+            <div className="flex flex-col items-center md:items-start ">
+                <Logo />
+                <div className="mt-3 text-zinc-400 text-center md:text-left">
+                    <div>Plataforma financeira</div>
+                    <div className="flex gap-1.5">que <span className="font-black text-transparent bg-clip-text bg-gradient-to-r from-indigo-600 to-cyan-600">simplifica</span> sua vida</div>
+                </div>
+            </div>
+            <div className="flex flex-col md:flex-row md:justify-between items-center mt-14">
+                <RedesSociais />
+                <p className="text-zinc-100 mt-7 md:mt-0 text-center">
+                <Link href="https://moser-portifolio.netlify.app" target="_blank">
+                    <span className="font-bold">M<span className="text-red-500 text-lg">O</span>SER</span> ® {new Date().getFullYear()} - Todos os direitos reservados
+                </Link>
+                </p>
+            </div>
         </Area>
     )
 }
